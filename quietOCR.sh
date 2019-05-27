cb24bb9108e86b84052831b6294c4aae155ba820
Nathan <Nathan-Nesbitt@users.noreply.github.com>
2019-05-27 16:19:59 -0700

    Small formatting fixes

--- quietOCR.sh
+++ quietOCR.sh
@@ -4,7 +4,7 @@
 #
 #          FILE:  quietOCR.sh
 #
-#         USAGE:  ./quietOCR.sh <inputFile> <outputFile>
+#         USAGE:  ./quietOCR.sh <inputFile>
 #
 #   DESCRIPTION:  OCR a file by creating a TIFF file and
 #
@@ -16,10 +16,10 @@
 #       CREATED:  2019-05-21
 #      REVISION:  R1
 #         NOTES:  This script is designed to OCR one file at a time. It can
-#				  be used to do a directory by specifying -a, but will run 
-#				  differently ./quietOCR.sh -a <fullDirectoryPath>. If you want
-#				  to get the text by itself, run it with the -n parameter after -a 
-#				  (or first for single file).
+#		  be used to do a directory by specifying -a, but will run 
+#		  differently ./quietOCR.sh -a <fullDirectoryPath>. If you want
+#		  to get the text by itself, run it with the -n parameter after -a 
+#		  (or first for single file).
 #
 #===============================================================================
 
